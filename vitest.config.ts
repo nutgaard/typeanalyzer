d8f31fed15e78febc67af804ce4ca4bc53f76230
Nicklas Utgaard <nutgaard@gmail.com>
2023-04-03 18:59:04 +0200

    Feat/workflow (#1)
    * ci: add github actions workflow
    
    * ci: adjust coverage threshold

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     test: {
-        include: ['src/**/*.test.{ts,mts,cts}']
+        include: ['src/**/*.test.{ts,mts,cts}'],
+        coverage: {
+            lines: 90,
+            functions: 90,
+            branches: 90,
+            statements: 90,
+        }
     },
 })
